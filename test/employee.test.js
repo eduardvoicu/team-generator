5a72cde4d0bae391e9225dc2465df27c87369b05
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-25 11:14:15 -0500

    finsished employee test js

--- test/employee.test.js
+++ test/employee.test.js
@@ -17,3 +17,43 @@ describe("Employee", () => {
         const e = new Employee("Foo", testValue);
         expect(e.id).toBe(testValue);
     });
+
+    it("Can set email via constructor argument", () => {
+        const testValue = "test@test.com";
+        const e = new Employee("Foo", 1, testValue);
+        expect(e.email).toBe(testValue);
+    });
+
+    describe("getName", () => {
+        it("Can get name via getName()", () => {
+            const testValue = "Alice";
+            const e = new Employee(testValue);
+            expect(e.getName()).toBe(testValue);
+        });
+    });
+        
+    describe("getId", () => {
+        it("Can get id via getId()", () => {
+            const testValue = 100;
+            const e = new Employee("Foo", testValue);
+            expect(e.getId()).toBe(testValue);
+        });
+    });
+
+    describe("getEmail", () => {
+        it("Can get email via getEmail()", () => {
+            const testValue = "test@test.com";
+            const e = new Employee("Foo", 1, testValue);
+            expect(e.getEmail()).toBe(testValue);
+        });
+    });
+        
+    describe("getRole", () => {
+        it("getRole() should return \"Employee\"", () => {
+            const testValue = "Employee";
+            const e = new Employee("Alice", 1, "test@test.com");
+            expect(e.getRole()).toBe(testValue);
+        });
+    });
+    
+});
